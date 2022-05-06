0a97a9892091b924585df16403d8b203aa328c3b
Ilfat <ilfat95@mail.ru>
2022-05-06 16:39:51 +0500

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                string stroka;
+                string color;
+                object userColor;
+
+                Console.WriteLine("Введите текст");
+                stroka = Console.ReadLine();
+                Console.WriteLine("Введите цвет (синий/красный)"); 
+                color = Console.ReadLine();
+                if (!Enum.TryParse(typeof(CustomColor), color, out userColor))
+                {
+                    userColor = Console.ForegroundColor;
+                }
+                Task2.Print(stroka, (int)userColor);
+            }
+        }
+    }
+    static class Task2
+    {
+        public static void Print(string stroka, int color)
+        {
+            Console.ForegroundColor = (ConsoleColor)color;
+            Console.WriteLine(stroka);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+    enum CustomColor
+    {
+        красный = 12,
+        синий = 9
+    }
+}
