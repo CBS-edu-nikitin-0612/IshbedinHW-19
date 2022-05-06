7a9bffa3bb3e5c2995d7daa4c48547d408cc69f0
Ilfat <ilfat95@mail.ru>
2022-05-06 17:00:35 +0500

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            foreach (Post worker in new [] { Post.Boss, Post.Staff})
+            {
+                foreach (int hoursh in new[] {25, 30, 45})
+                {
+                    Console.WriteLine(String.Format("Должность {0,7} часы {1,4} повышение {2}", worker, hoursh, Accountant.AskForBonus(worker, hoursh)));
+                }
+            }
+            //Delay
+            Console.ReadKey();
+        }
+    }
+
+    class Accountant
+    {
+        public static bool AskForBonus(Post worker, int hours)
+        {
+            return hours > (int)worker;
+        }
+    }
+
+    enum Post
+    {
+        Boss = 40,
+        Staff = 30
+    }
+}
